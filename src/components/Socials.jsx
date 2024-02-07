ec0b1de1e153d4846d9b8cd1b315df3319d181c3
SisterMichelle <mdnewman@proton.me>
2024-02-07 18:39:26 +0000

    FIX x-logo

--- src/components/Socials.jsx
+++ src/components/Socials.jsx
@@ -1,7 +1,7 @@
 import { useRef } from 'react';
 
 import email from "../assets/icons/email.svg";
-import twitter from "../assets/icons/twitter-original.svg";
+import xlogo from "../assets/icons/x-logo-black.png";
 import linkedin from "../assets/icons/linkedin-plain.svg";
 import github from "../assets/icons/github-original.svg";
 
@@ -20,7 +20,7 @@ export default function Socials({ isOpen }) {
             <ul ref={socials} id="socials" className={classList}>
                 <li><a href="mailto:mdnewman@proton.me" target="_blank"><img className="icon" src={email} alt="Email Me" /></a></li>
                 <li><a href="https://www.linkedin.com/in/michellediannenewman/" target="_blank"><img className="icon" src={linkedin}  alt="Connect with me on LinkedIn" /></a></li>
-                <li><a href="http://x.com/michlalatweets" target="_blank"><img className="icon" src={twitter} alt="Tweets on X" /></a></li>
+                <li><a href="http://x.com/michlalatweets" target="_blank"><img className="icon" src={xlogo} alt="Tweets on X" /></a></li>
                 <li><a href="https://github.com/SisterMichelle" target="_blank"><img className="icon" src={github} alt="My GitHub Repos" /></a></li>
             </ul>
         </div>
